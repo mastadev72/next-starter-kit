183d78b6ade5cf3fdb4fdf915caa9bfe5985a944
MastADEV <devstuv@gmail.com>
2024-02-15 12:04:38 +0100

    comm 01

---
+++ app/about/team/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>Team</div>
+    )
+}
+
+export default Page;
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import styles from './page.module.css'
+import '@radix-ui/themes/styles.css';
 
 export default function Home() {
   return (
